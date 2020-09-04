46c660b6899ef59dea7d896e59250f5b27c0a354
mathewmorris <hello@mathewmorris.com>
2020-09-04 04:40:48 -0500

    Small updates

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import React from 'react';
 
-export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
+const App = () => {
+  const [username, setUsername] = React.useState();
+  React.useEffect(() => {
     fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+      .then((res) => res.json())
+      .then((user) => setUsername(user.username));
+  }, []);
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {username ? (
+        <h1>{`Hello ${username}`}</h1>
+      ) : (
+        <h1>Loading.. please wait!</h1>
+      )}
+    </div>
+  );
+};
+
+export default App;
